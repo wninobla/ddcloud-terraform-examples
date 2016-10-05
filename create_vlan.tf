418f929757a976986ba282aea29df2113ace3654
wninobla <william.ninobla@dimensiondata.com>
2016-10-05 11:38:18 -0700

    Added create_vlan.tf file

---
+++ create_vlan.tf
@@ -0,0 +1,63 @@
+/*
+ * The section below build on the configuration files in create_networkdomain and includes resource
+ * definitions to create networks via Terraform.  The information below is meant to be added to the
+ * existing file but is separated for learning purposes.
+ * 
+ */
+
+#  Copy and paste the section below into the create_networkdomain.tf file created previously 
+ 
+#  "Resource" section for "ddcloud_vlan" which defines how Terraform builds infrastructure assets.
+#  This resource creates network VLANs.
+
+resource "ddcloud_vlan" "dmz-vlan" {
+  name                 = "DMZ"
+  description          = "This is an automated Terraform VLAN designated for DMZ hosts"
+  networkdomain        = "${ddcloud_networkdomain.networkdomain.id}"
+  ipv4_base_address    = "192.168.1.0"
+  ipv4_prefix_size     = 24
+  depends_on           = ["ddcloud_networkdomain.networkdomain"]
+}
+
+resource "ddcloud_vlan" "trust-vlan" {
+  name                 = "TRUST"
+  description          = "This is an automated Terraform VLAN designated for TRUST hosts"
+  networkdomain        = "${ddcloud_networkdomain.networkdomain.id}"
+  ipv4_base_address    = "192.168.2.0"
+  ipv4_prefix_size     = 24
+  depends_on           = ["ddcloud_networkdomain.networkdomain"]
+}
+
+#  "Resource" Section Notes:
+#  
+#  1.  The networkdomain field makes use of Terraform's HCL language and references a variable 
+#      for in this case the ID field that the cloud API assigned to the network domain created
+#      previously.
+#  2.  The ipv4_base_address defines the network block associated with the newtwork VLAN being
+#      created.
+#  3.  The ipv4_prefix_size defines the CIDR block size being defined.  This has to follow RFC1918
+#      guidelines in otder to be valid.
+#  4.  The depends_on field sets a dependency on the network domain being crated BEFORE executing
+#      the resources defined here for VLAN creation.
+#  5.  If nothing is changes, the resource definition will create (2) network VLANs with the
+#      following definitions:
+#
+#      + ddcloud_vlan.dmz-vlan
+#          description:       "This is an automated Terraform VLAN designated for DMZ hosts"
+#          ipv4_base_address: "192.168.1.0"
+#          ipv4_prefix_size:  "24"
+#          ipv6_base_address: "<computed>"
+#          ipv6_prefix_size:  "<computed>"
+#          name:              "DMZ"
+#          networkdomain:     "6a0390aa-da05-47dd-a535-17434b8d409b"
+#
+#      + ddcloud_vlan.trust-vlan
+#          description:       "This is an automated Terraform VLAN designated for TRUST hosts"
+#          ipv4_base_address: "192.168.2.0"
+#          ipv4_prefix_size:  "24"
+#          ipv6_base_address: "<computed>"
+#          ipv6_prefix_size:  "<computed>"
+#          name:              "TRUST"
+#          networkdomain:     "6a0390aa-da05-47dd-a535-17434b8d409b"
+#
+#  6.  For more information on resource usage, please go to the URL - https://goo.gl/bax4qD
